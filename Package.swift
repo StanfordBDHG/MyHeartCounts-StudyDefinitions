cf8147f1db7bb570b0dec5743d23e735d221e160
Lukas Kollmer <hey@lukaskollmer.de>
2025-10-16 18:32:57 +0200

    fix heart risk survey

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .executable(name: "MHCStudyDefinitionExporterCLI", targets: ["MHCStudyDefinitionExporterCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/StanfordSpezi/SpeziStudy.git", from: "0.1.14"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziStudy.git", revision: "116a197947a818c3075d21638d0f43bbc413fe3d"),
         .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.4.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.2")
     ],
