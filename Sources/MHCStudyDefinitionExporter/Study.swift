ab2de77fd7a4a6aada6ac17d7bbc81c44c360a0f
Lukas Kollmer <hey@lukaskollmer.de>
2026-02-01 15:41:16 +0100

    add spanish welcome text (#27)
    # add spanish welcome text
    
    ## :recycle: Current situation & Problem
    we're currently missing the spanish translation of the "Welcome to My
    Heart Counts" article.
    
    ## :gear: Release Notes
    - add spanish welcome text
    
    ### Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

--- Sources/MHCStudyDefinitionExporter/Study.swift
+++ Sources/MHCStudyDefinitionExporter/Study.swift
@@ -38,7 +38,7 @@ extension StudyBundle.FileReference {
 
 
 let mhcStudyDefinition = StudyDefinition(
-    studyRevision: 36,
+    studyRevision: 37,
     metadata: .init(
         id: .mhcStudy,
         title: "My Heart Counts",
